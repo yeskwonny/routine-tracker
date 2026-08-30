5751c123f69c90287cf93a07f71c5e9cdca50523
yesol <yeahsol.kwon@gmail.com>
2026-08-30 19:22:09 +1000

    connect to DB

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,10 +2,23 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Item } from "./entities/Item.js";
 
-export const AppDataSource = new DataSource({
-  type: "better-sqlite3",
-  database: "database.sqlite",
-  synchronize: true,
-  logging: true,
-  entities: [Item],
-});
+const isProduction = !!process.env.DATABASE_URL;
+
+export const AppDataSource = new DataSource(
+  isProduction
+    ? {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        synchronize: true,
+        logging: true,
+        entities: [Item],
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        type: "better-sqlite3",
+        database: "database.sqlite",
+        synchronize: true,
+        logging: true,
+        entities: [Item],
+      },
+);
